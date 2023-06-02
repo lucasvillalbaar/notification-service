c0d9b864efbf497d01bc40d17a8fe1d5ed60878a
Lucas Villalba <37760788+lucasvillalbaar@users.noreply.github.com>
2023-06-02 19:34:57 -0300

    refactor to properly handle background notifications

--- mobile/AwesomeProject/App.tsx
+++ mobile/AwesomeProject/App.tsx
@@ -3,12 +3,27 @@ import { View, Text, StyleSheet, Alert } from 'react-native';
 import {PermissionsAndroid} from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 
-export default function App() {
-  const [title, setNotificationTitle] = useState('Title');
-  const [body, setNotificationBody] = useState('Body');
+async function saveTokenToDatabase(token:string) {
+  // TODO: Save token into the database
+  console.log('TOKEN SAVED');
+}
+
+type AppProps = {
+  title: string;
+  setNotificationTitle: (title: string) => void;
+  body: string;
+  setNotificationBody: (body: string) => void;
+};
+
+export default function App({
+  title,
+  setNotificationTitle,
+  body,
+  setNotificationBody,
+}: AppProps) {
   
+  // Handle foreground messages
   useEffect(() =>{
-    // Handle foreground messages
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       console.log('MESSAGE HANDLED IN FOREGROUND');
       console.log(remoteMessage.notification?.title);
@@ -23,19 +38,27 @@ export default function App() {
     return unsubscribe;
   }, []);
   
+  // Get Firebase token and save it
   useEffect(() => {
     //Get the token from Firebase and display it in the console (for development purposes only)
     messaging()
     .getToken()
     .then((token) => {
       console.log('TOKEN:', token);
-      // TODO: Send token to the backend
+      return saveTokenToDatabase(token);
     })
     .catch((error) => {
       console.log('ERROR: Error al obtener el token de Firebase:', error);
     });
 
-    // Request notification permissions from the user
+    // Listen to whether the token changes
+    return messaging().onTokenRefresh(token => {
+      saveTokenToDatabase(token);
+      });
+  },[]);
+
+  // Request notification permissions from the user
+  useEffect(() => {
     const requestNotificationPermission = async () => {
       try {
         const granted = await PermissionsAndroid.request(
@@ -47,14 +70,14 @@ export default function App() {
           },
           );
 
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log('Permisos de notificación concedidos');
-        } else {
-          console.log('Permisos de notificación denegados');
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log('Notification permissions granted');
+          } else {
+            console.log('Notification permissions denied');
+          }
+        } catch (error) {
+          console.error('Error requesting notification permissions:', error);
         }
-      } catch (error) {
-        console.error('Error al solicitar permisos de notificación:', error);
-      }
     };
 
     // Request notification permissions from the user when loading the application
--- mobile/AwesomeProject/index.js
+++ mobile/AwesomeProject/index.js
@@ -1,18 +1,34 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import messaging from '@react-native-firebase/messaging';
+import React, { useState } from 'react';
+
+const AppWrapper = () => {
+  const [title, setNotificationTitle] = useState('Title');
+  const [body, setNotificationBody] = useState('Body');
 
   // Handle background messages
-  messaging().setBackgroundMessageHandler(async remoteMessage => {
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
     console.log('MESSAGE HANDLED IN BACKGROUND');
     console.log(remoteMessage.notification?.title);
     console.log(remoteMessage.notification?.body);
     // Do something with the data received
+    const remoteTitle = remoteMessage.notification?.title ?? 'Title';
+    const remoteBody = remoteMessage.notification?.body ?? 'Body';
+    setNotificationTitle(remoteTitle);
+    setNotificationBody(remoteBody);
   });
 
-AppRegistry.registerComponent(appName, () => App);
+  return (
+    <App
+      title={title} // Pass the 'title' state as a prop to App component
+      setNotificationTitle={setNotificationTitle} // Pass the 'setNotificationTitle' function as a prop to App component
+      body={body} // Pass the 'body' state as a prop to App component
+      setNotificationBody={setNotificationBody} // Pass the 'setNotificationBody' function as a prop to App component
+    />
+  );
+};
+
+// Register the AppWrapper component instead of the App component
+AppRegistry.registerComponent(appName, () => AppWrapper);
