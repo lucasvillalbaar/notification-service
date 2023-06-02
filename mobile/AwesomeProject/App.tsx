de551486c74557971f930dd7103ab5e7dbf49d98
Lucas Villalba <37760788+lucasvillalbaar@users.noreply.github.com>
2023-06-02 17:35:56 -0300

    request permissions to show notifications

--- mobile/AwesomeProject/App.tsx
+++ mobile/AwesomeProject/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import {PermissionsAndroid} from 'react-native';
 
 export default function App() {
+  useEffect(() => {
+    // Request notification permissions from the user
+    const requestNotificationPermission = async () => {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+          {
+            title: 'Permisos de notificación',
+            message: 'La aplicación necesita acceder a las notificaciones para recibir notificaciones push.',
+            buttonPositive: 'Aceptar',
+          },
+        );
+
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('Permisos de notificación concedidos');
+        } else {
+          console.log('Permisos de notificación denegados');
+        }
+      } catch (error) {
+        console.error('Error al solicitar permisos de notificación:', error);
+      }
+    };
+
+    // Request notification permissions from the user when loading the application
+    requestNotificationPermission();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Title</Text>
