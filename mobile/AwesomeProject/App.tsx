263da8b5dc1ff48df39f77546ac1dfe1b0f7cfb9
Lucas Villalba <37760788+lucasvillalbaar@users.noreply.github.com>
2023-06-02 17:52:05 -0300

    adding handlers for notifications and shows it on screen

--- mobile/AwesomeProject/App.tsx
+++ mobile/AwesomeProject/App.tsx
@@ -1,8 +1,42 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
 import {PermissionsAndroid} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 export default function App() {
+  const [title, setNotificationTitle] = useState('Title');
+  const [body, setNotificationBody] = useState('Body');
+  
+  //Get the token from Firebase and display it in the console (for development purposes only)
+  messaging()
+  .getToken()
+  .then((token) => {
+    console.log('TOKEN:', token);
+    // TODO: Send token to the backend
+  })
+  .catch((error) => {
+    console.log('ERROR: Error al obtener el token de Firebase:', error);
+  });
+
+  // Handle background messages
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('MESSAGE HANDLED IN BACKGROUND');
+    console.log(remoteMessage.notification?.title);
+    console.log(remoteMessage.notification?.body);
+  });
+
+  // Handle foreground messages
+  messaging().onMessage(async remoteMessage => {
+    console.log('MESSAGE HANDLED IN FOREGROUND');
+    console.log(remoteMessage.notification?.title);
+    console.log(remoteMessage.notification?.body);
+
+    const remoteTitle = remoteMessage.notification?.title ?? 'Title';
+    const remoteBody = remoteMessage.notification?.body ?? 'Body';
+    setNotificationTitle(remoteTitle);
+    setNotificationBody(remoteBody);
+  });
+
   useEffect(() => {
     // Request notification permissions from the user
     const requestNotificationPermission = async () => {
@@ -32,8 +66,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Title</Text>
-      <Text style={styles.body}>Body</Text>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.body}>{body}</Text>
     </View>
   );
 }
